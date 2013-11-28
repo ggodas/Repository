cb709a0ab82f857a25d75e06ae4623519879a8f6
ggodas <george.godas@ctf.com.br>
2013-11-28 14:28:05 -0200

    Add transactional attribute

---
+++ SoftSize.Infrastructure.MVC/Filters/NHibernateSessionAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate;
+using NHibernate.Context;
+
+
+namespace SoftSize.Infrastructure.MVC
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class NHibernateSessionFactor : System.Web.Http.Filters.ActionFilterAttribute
+    {
+        ISessionFactory sessionFactory;
+
+        protected ISessionFactory SessionFactory
+        {
+            get
+            {
+                return sessionFactory;
+            }
+        }
+
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            var session = sessionFactory.OpenSession();
+            CurrentSessionContext.Bind(session);
+        }
+
+        public override void OnActionExecuted(System.Web.Http.Filters.HttpActionExecutedContext actionExecutedContext)
+        {
+            var session = CurrentSessionContext.Unbind(sessionFactory);
+            session.Close();
+        }
+    }
+}
---
+++ SoftSize.Infrastructure.MVC/Filters/NeedsPersistenseAttribute.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NHibernate;
+
+namespace SoftSize.Infrastructure.MVC
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class NeedsPersistenseAttribute : NHibernateSessionFactor
+    {
+        private bool transactional;
+        public NeedsPersistenseAttribute(bool transactional = false)
+        {
+            this.transactional = transactional;
+        }
+        protected ISession session
+        {
+            get
+            {
+                return SessionFactory.GetCurrentSession();
+            }
+        }
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            base.OnActionExecuting(actionContext);
+            if (transactional)
+                session.BeginTransaction();
+        }
+        public override void OnActionExecuted(System.Web.Http.Filters.HttpActionExecutedContext actionExecutedContext)
+        {
+            if (transactional)
+            {
+                var tx = session.Transaction;
+                if (actionExecutedContext.Exception == null && tx != null && tx.IsActive)
+                    try
+                    {
+                        session.Transaction.Commit();
+                    }
+                    catch
+                    {
+                        session.Transaction.Rollback();
+                        throw;
+                    }
+                else
+                    session.Transaction.Rollback();
+
+            }
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
